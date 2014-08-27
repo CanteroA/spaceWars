12b8ce4953237bb2ba9950964ca7ddd615a73255
unknown <PC3SB-3@pc3sb-3.USB.LOCAL>
2014-08-26 20:20:53 -0500

    Añadiendo funciones al graphicObjects

--- SourceCode/spaceWars/graphicobject.cpp
+++ SourceCode/spaceWars/graphicobject.cpp
@@ -2,7 +2,7 @@
 
 graphicObject::graphicObject()
 {
-    _direction = 0;
+    _direction = DIR_RIHT;
     _pos=point(0,0);
     //Creamos gráfico cuando la posición es para la derecha
     _grapRight.append(graphicChar(point(0,0), '?'));
@@ -17,40 +17,148 @@ graphicObject::graphicObject()
     _grapUp.append(graphicChar(point(0,1), '?'));
 
     //Creamos gráfico cuando la posición es para abajo
-    _grapdown.append(graphicChar(point(0,0), '?'));
-    _grapdown.append(graphicChar(point(0,1), 'V'));
+    _grapDown.append(graphicChar(point(0,0), '?'));
+    _grapDown.append(graphicChar(point(0,1), 'V'));
 
     //Inicializar la velocidad en 0
     _speed = 0;
 
     //Definir la medida de los universos
-
     _fieldLimitDown = 0;
     _fieldLimitLeft = 0;
     _fieldLimitRight = 0;
     _fieldLimitTop = 0;
 
+    //Looking for hit area for the objects
 
-    ///Definir el Hit Area
-    ///Definir el el edgeLimit que es el límite de la nave
+    hitAreaRight.append(point(0,0));
+    hitAreaRight.append(point(1,0));
+    hitAreaLeft.append(point(0,0));
+    hitAreaLeft.append(point(1,0));
+    hitAreaDown.append(point(0,0));
+    hitAreaDown.append(point(0,1));
+    hitAreaUp.append(point(0,0));
+    hitAreaUp.append(point(0,1));
 
+    _edgeRight = 1;
+    _edgeLeft = 0;
+    _edgeTop = 0;
+    _edgeDown = 0;
 
+    ///Valores para la derecha
 
-
-
-
-
-
+    _hitPower = 1;
 
 }
+
+///Definir el Hit Area
+///Definir el el edgeLimit que es el límite de la nave
+
 
 int graphicObject::paint()
 {
+    switch(_direction)
+    {
+    case DIR_RIHT:
+        for(int i = 0; i<_grapRight.size();i++)
+            _grapRight[i].paint(_pos);
+        break;
+    case DIR_LEFT:
+        for(int i = 0; i<_grapLeft.size();i++)
+            _grapLeft[i].paint(_pos);
+        break;
+    case DIR_UP:
+        for(int i = 0; i<_grapUp.size();i++)
+            _grapUp[i].paint(_pos);
+        break;
+    case DIR_DOWN:
+        for(int i = 0; i<_grapDown.size();i++)
+            _grapDown[i].paint(_pos);
+        break;
 
+    default:
+        return -1;
+        ///Dirección no válida
+    }
+    return 0;
+    /// Buen trabajo de número
 }
-
+//Verifica que el objeto no se pase de las margenes
 int graphicObject::tic(double time)
 {
+    point newPos;
+    //Intento moverme
+    switch (_direction) {
+    case DIR_RIHT:
+        newPos = point(_pos.x()+ _speed*time, _pos.y());
+        break;
+    case DIR_LEFT:
+        newPos = point(_pos.x()- _speed*time, _pos.y());
+        break;
+    case DIR_UP:
+        newPos = point(_pos.x(), _pos.y()- _speed*time);
+        break;
+
+    case DIR_DOWN:
+        newPos = point(_pos.x(), _pos.y()+ _speed*time);
+        break;
+
+    default:
+        return -1;
+        break;
+    }
+
+    //Verificar si está dentro del universo
+    bool inside=true;
+    switch (_direction) {
+    case DIR_RIHT:
+        if(newPos.x() + _edgeRight >= _fieldLimitRight)
+            inside = false;
+        break;
+    case DIR_LEFT:
+        if(newPos.x() <= _fieldLimitLeft)
+            inside = false;
+        break;
+    case DIR_UP:
+        if(newPos.y() <= _fieldLimitTop)
+            inside = false;
+        break;
+    case DIR_DOWN:
+        if(newPos.y() + _edgeDown >= _fieldLimitDown)
+            inside = false;
+        break;
+    default:
+        return -1;
+        break;
+    }
+    if(inside = true)
+    {
+        _pos = newPos;                //Si la nave está dentro
+    }
+    else
+    {
+        _speed=0;                    //Si la nave se salió
+        //Detengo la nave
+        switch (_direction) {
+        //La llevo al límite
+        case DIR_RIHT:
+            _pos.setX(_fieldLimitRight-_edgeRight-1);
+            break;
+        case DIR_DOWN:
+            _pos.setY(_fieldLimitDown-_edgeDown-1);
+            break;
+        case DIR_LEFT:
+            _pos.setX(_fieldLimitLeft + 1);
+            break;
+        case DIR_UP:
+            _pos.setY(_fieldLimitTop + 1);
+            break;
+
+        default:
+            break;
+        }
+
+    }
 
 }
 
--- SourceCode/spaceWars/graphicobject.h
+++ SourceCode/spaceWars/graphicobject.h
@@ -4,23 +4,29 @@
 #include "graphicchar.h"
 #include "basicGraphic/basicgraphic.h"
 #include "qlist.h"
+#define DIR_RIHT 0
+#define DIR_LEFT 2
+#define DIR_UP 1
+#define DIR_DOWN 3
+
+
 class graphicObject
 {
 private:
     char _direction;
     point _pos;
-    QList<graphicChar>_grapRight,_grapLeft ,_grapUp,_grapdown;
+    QList<graphicChar>_grapRight,_grapLeft ,_grapUp,_grapDown;
     float _speed;
     int _fieldLimitRight, _fieldLimitLeft, _fieldLimitDown, _fieldLimitTop;
     QList<point>hitAreaRight, hitAreaLeft, hitAreaDown, hitAreaUp;
     unsigned char _keyPosRight, _keyPosLeft, _keyPosUp, _keyPosDown;
-    //int _edgeRight, _edgeLeft, _edgeTop, _edgeDown;
+    int _edgeRight, _edgeLeft, _edgeTop, _edgeDown;
     int _hitPower;
 public:
     //initialize all members
     graphicObject();
     int paint();
-    int tic(double time);
+    int tic(double time); //Dado en milisegundos
     int checkHit(point p);
     int checkHit(QList<point>points);
 };
